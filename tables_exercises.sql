f69c3e7f3a26a5967b7dbb9092d10e8eb2ed86e8
Gabriela Tijerina <gabriela.i.tijerina@gmail.com>
2020-12-16 18:58:34 -0600

    new table exercises

---
+++ tables_exercises.sql
@@ -0,0 +1,15 @@
+USE employees;
+
+SHOW tables;
+
+DESCRIBE employees; 
+
+DESCRIBE employees_with_departments;
+DESCRIBE salaries;
+DESCRIBE titles;
+DESCRIBE departments;
+DESCRIBE dept_manager;
+
+SHOW CREATE TABLE dept_manager;
+
+
