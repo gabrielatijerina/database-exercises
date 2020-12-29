22f46ec3323f783523376f84ca41962feb7d9807
Gabriela Tijerina <gabriela.i.tijerina@gmail.com>
2020-12-29 11:02:34 -0600

    updated exercises

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,42 +1,34 @@
 USE albums_db;
 
-SELECT database();
-
 SHOW tables;
 
 DESCRIBE albums;
 
 SELECT *
-FROM albums 
-LIMIT 5;
-
-SELECT DISTINCT genre
-from albums;
-
-SELECT name AS 'Albums by Pink Floyd'
 FROM albums WHERE artist = 'Pink Floyd';
 
-SELECT *
+SELECT release_date as "Sgt Pepper\'s Release Date"
 FROM albums 
-WHERE name = 'sgt. pepper\'s lonely hearts club band';
+WHERE name = 'Sgt. Pepper\'s Lonely Hearts Club Band';
 
-SELECT genre AS 'Genre for Nevermind'
-FROM albums
-WHERE name = 'nevermind';
+SELECT genre as 'Genre for Nevermind'
+FROM albums 
+WHERE name = 'Nevermind';
 
-SELECT artist, 
-	name AS 'Albums Released in the 1990s'
-FROM albums
-WHERE release_date like '199%';
+SELECT artist,
+	name as 'Albums Released in 1990s'
+FROM albums 
+WHERE release_date BETWEEN 1990 AND 1999;
+
+SELECT name as 'Album Sales Less Than 20 million'
+from albums
+WHERE sales < 20;
 
 SELECT * 
 FROM albums 
-WHERE sales < 20;
+WHERE genre = 'Rock';
 
-SELECT *
-FROM albums 
-WHERE genre = 'rock';
-
-SELECT *
+SELECT * 
 FROM albums 
 WHERE genre like '%rock%';
+
